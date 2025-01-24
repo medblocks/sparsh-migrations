035d4822548da119f48cd836310df00ffdc2dc9d
neutralboy <leopoorna99@gmail.com>
2025-01-24 08:13:35 +0000

    Update SQL database pipeline to include incremental loading for 'adt_discharge'

--- sql_database_pipeline.py
+++ sql_database_pipeline.py
@@ -12,6 +12,7 @@ from sqlalchemy.sql.sqltypes import TypeEngine
 import sqlalchemy as sa
 from prefect import flow
 
+
 @flow(log_prints=True, name="MHEA Phase 1 pipeline")
 def create_mhea_pipeline():
     pipeline = dlt.pipeline(
@@ -19,7 +20,7 @@ def create_mhea_pipeline():
 
     # Configure the source to load a few select tables increentally
     source_1 = sql_database().with_resources("Registration", "RegistrationAddress", "AM_State",
-                                             "AM_Nationality", "AM_City", "emr_referraldetails", "__DoctorSpeciality", "BT_Encounter", "ADT_PatientBedStatus", "AM_Bed", "ADT_Admission", "BT_Invoice", "adt_discharge","__CompanyPayorCategory", "adt_discharge_diagnosis")
+                                             "AM_Nationality", "AM_City", "emr_referraldetails", "__DoctorSpeciality", "BT_Encounter", "ADT_PatientBedStatus", "AM_Bed", "ADT_Admission", "BT_Invoice", "adt_discharge", "__CompanyPayorCategory", "adt_discharge_diagnosis")
 
     # Add incremental config to the resources. "updated" is a timestamp column in these tables that gets used as a cursor
     source_1.Registration.apply_hints(
@@ -31,6 +32,8 @@ def create_mhea_pipeline():
     source_1.ADT_Admission.apply_hints(
         incremental=dlt.sources.incremental("EnteredDate"))
     source_1.BT_Invoice.apply_hints(
+        incremental=dlt.sources.incremental("EnteredDate"))
+    source_1.adt_discharge.apply_hints(
         incremental=dlt.sources.incremental("EnteredDate"))
 
     # Run the pipeline. The merge write disposition merges existing rows in the destination by primary key
