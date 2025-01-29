dd5d49c3962f978cf1d5c09cf6a25c0ccbcb6f33
neutralboy <leopoorna99@gmail.com>
2025-01-29 13:47:45 +0000

    Add incremental loading for additional tables and create master mapping pipeline

--- sql_database_pipeline.py
+++ sql_database_pipeline.py
@@ -20,7 +20,7 @@ def create_mhea_pipeline():
 
     # Configure the source to load a few select tables increentally
     source_1 = sql_database().with_resources("Registration", "RegistrationAddress", "AM_State",
-                                             "AM_Nationality", "AM_City", "emr_referraldetails", "__DoctorSpeciality", "BT_Encounter", "ADT_PatientBedStatus", "AM_Bed", "ADT_Admission", "BT_Invoice", "adt_discharge", "__CompanyPayorCategory", "visit_assessments","BT_ServiceOrderMain","BT_ServiceOrderDtl")
+                                             "AM_Nationality", "AM_City", "emr_referraldetails", "__DoctorSpeciality", "BT_Encounter", "ADT_PatientBedStatus", "AM_Bed", "ADT_Admission", "BT_Invoice", "adt_discharge", "__CompanyPayorCategory", "visit_assessments","BT_ServiceOrderMain","BT_ServiceOrderDtl", "AM_Employee", "AM_Facility", "AM_EmployeeFacilityMappingDoNotDelete", "AM_EmployeeDoNotDelete")
 
     # Add incremental config to the resources. "updated" is a timestamp column in these tables that gets used as a cursor
     source_1.Registration.apply_hints(
@@ -41,11 +41,32 @@ def create_mhea_pipeline():
         incremental=dlt.sources.incremental("EnteredDate"))
     source_1.BT_ServiceOrderDtl.apply_hints(
         incremental=dlt.sources.incremental("EnteredDate"))
+    source_1.AM_Employee.apply_hints(
+        incremental=dlt.sources.incremental("EnteredDate"))
+    source_1.AM_Facility.apply_hints(
+        incremental=dlt.sources.incremental("EnteredDate"))
+    source_1.AM_EmployeeFacilityMappingDoNotDelete.apply_hints(
+        incremental=dlt.sources.incremental("EnteredDate"))
+    source_1.AM_EmployeeDoNotDelete.apply_hints(
+        incremental=dlt.sources.incremental("EnteredDate"))
 
     # Run the pipeline. The merge write disposition merges existing rows in the destination by primary key
     info = pipeline.run(source_1, write_disposition="merge")
     print(info)
 
+@flow(log_prints=True, name="MHEA Master mapping pipeline")
+def create_master_pipeline():
+    pipeline2 = dlt.pipeline(
+        pipeline_name="mhea_master", destination='postgres', dataset_name="mhea_master")
+
+    source_2 = sql_database().with_resources("__AM_Employee", "__SARSH_Surgery_MASTER20240118", "__SparshServiceMaster20240120", "__SparshTariff23102024", "__Sparsh_Employee_Master", "__Sparsh_Specialization", "__Sparsh_Nomenclature", "__SParshEquipmentCharges")
+
+    source_2.__AM_Employee.apply_hints(
+        incremental=dlt.sources.incremental("EnteredDate"))
+
+    info = pipeline2.run(source_2, write_disposition="merge")
+    print(info)
 
 if __name__ == "__main__":
+    create_master_pipeline()
     create_mhea_pipeline()
