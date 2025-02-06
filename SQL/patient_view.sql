2e8f5ba6ab208030b5255804594323bdfd21b356
neutralboy <leopoorna99@gmail.com>
2025-02-06 23:00:45 +0530

    Refactor patient_view SQL script to use LEFT JOINs and add enterprise_id filter

--- SQL/patient_view.sql
+++ SQL/patient_view.sql
@@ -1,5 +1,4 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS public.patient_view
-AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.patient_view AS
 SELECT
   r.id AS patient_id,
   r.registration_date AS registration_date,
@@ -10,18 +9,19 @@ SELECT
   r.email AS email,
   r.mobile_no AS mobile_no,
   ra.present_pin_code AS pin,
-  CONCAT(ra.present_street, ' ', ra.present_flat_house_no) AS address,
+  CONCAT (ra.present_street, ' ', ra.present_flat_house_no) AS address,
   tc.description as district,
   ts.description as state,
   tn.description as country
-FROM mhea_replica.registration AS r
-INNER JOIN mhea_replica.registration_address AS ra
-  ON r.id = ra.registration_id
-JOIN mhea_replica.am_city as tc
- on ra.present_city_id = tc.id
-JOIN mhea_replica.am_state as ts
-	on ra.present_state_id = ts.id
-JOIN mhea_replica.am_nationality as tn
-	on ra.present_country_id = tn.id
-WITH NO DATA;
+FROM
+  mhea_replica.registration AS r
+  LEFT JOIN mhea_replica.registration_address AS ra ON r.id = ra.registration_id
+  LEFT JOIN mhea_replica.am_city as tc on ra.present_city_id = tc.id
+  LEFT JOIN mhea_replica.am_state as ts on ra.present_state_id = ts.id
+  LEFT JOIN mhea_replica.am_nationality as tn on ra.present_country_id = tn.id
+where
+  r.enterprise_id = 8
+WITH
+  NO DATA;
+
 REFRESH MATERIALIZED VIEW public.patient_view;
